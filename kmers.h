f1be7371b53983419c538b2eb936a1b5579422ed
Rastislav Rabatin <rastislav.rabatin@gmail.com>
2016-03-25 23:16:30 +0100

    Class for KmerWindowIterator.

--- kmers.h
+++ kmers.h
@@ -23,4 +23,45 @@ IntType encodeKmer(const std::string& kmer);
 template <typename IntType>
 std::string decodeKmer(IntType code);
 
+/*
+template <typename IntType>
+IntType moveKmerWindow(int, IntType this_window_code,
+                       std::string::iterator *begin_window,
+                       std::string::iterator *end_window,
+                       std::string::iterator *string_end) {
+    if (end_window == string_end) return (IntType)-1;
+
+    IntType first_val = **begin_window;
+    IntType new_val = **end_window;
+}
+*/
+
+// Class that represents sliding window moving through the given string.
+template <typename IntType>
+class KmerWindowIterator {
+ public:
+  // end_window points one elements after the last element in the window.
+  KmerWindowIterator(int k, const std::string::iterator& begin_window,
+                     const std::string::iterator& end_window,
+                     const std::string::iterator& string_end)
+      : k_(k),
+        begin_window_(begin_window),
+        end_window_(end_window),
+        string_end_(string_end) {}
+
+  bool hasNext() {
+    if (end_window_ == string_end_) return true;
+    return false;
+  }
+  // Returns encoded kmer that is in the current window or -1 in case we are at
+  // the end of the string.
+  IntType next();
+  // Return string representation of current kmer.
+  std::string currentKmer() { return }
+
+ private:
+  int k_;  // Length of kmer.
+  std::string::iterator begin_window_, end_window_, string_end_;
+};
+
 #include "kmers.inl"
