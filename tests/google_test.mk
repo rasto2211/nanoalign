d67eed276294f7c57159ecec68fd5a9592fa0eb6
Rastislav Rabatin <rastislav.rabatin@gmail.com>
2016-04-08 15:07:32 +0200

    Added gmock to google makefile.

--- tests/google_test.mk
+++ tests/google_test.mk
@@ -1,20 +1,28 @@
 # Points to the root of Google Test, relative to where this file is.
 # Remember to tweak this if you move this file.
 GTEST_DIR = /usr/src/gtest
+GMOCK_DIR = /usr/src/gmock
 
 # Flags passed to the preprocessor.
 # Set Google Test's header directory as a system directory, such that
 # the compiler doesn't generate warnings in Google Test headers.
-CPPFLAGS += -isystem $(GTEST_DIR)/include
+CPPFLAGS += -isystem $(GTEST_DIR)/include -isystem $(GMOCK_DIR)/include
 
 # All Google Test headers.  Usually you shouldn't change this
 # definition.
 GTEST_HEADERS = /usr/include/gtest/*.h \
                 /usr/include/gtest/internal/*.h
 
+# All Google Mock headers. Note that all Google Test headers are
+# included here too, as they are #included by Google Mock headers.
+# Usually you shouldn't change this definition.	
+GMOCK_HEADERS = /usr/include/gmock/*.h \
+                /usr/include/gmock/internal/*.h \
+
 # Usually you shouldn't tweak such internal variables, indicated by a
 # trailing _.
 GTEST_SRCS_ = $(GTEST_DIR)/src/*.cc $(GTEST_DIR)/src/*.h $(GTEST_HEADERS)
+GMOCK_SRCS_ = $(GMOCK_DIR)/src/*.cc $(GMOCK_HEADERS) $(GTEST_HEADERS)
 
 # For simplicity and to avoid depending on Google Test's
 # implementation details, the dependencies specified below are
@@ -32,4 +40,18 @@ tests/gtest.a : tests/gtest-all.o
 	$(AR) $(ARFLAGS) $@ $^
 
 tests/gtest_main.a : tests/gtest-all.o tests/gtest_main.o
+	$(AR) $(ARFLAGS) $@ $^
+
+tests/gmock-all.o : $(GMOCK_SRCS_)
+	$(CXX) $(CPPFLAGS) -I$(GTEST_DIR) -I$(GMOCK_DIR) $(CXXFLAGS) \
+            -c $(GMOCK_DIR)/src/gmock-all.cc -o tests/gmock-all.o
+
+tests/gmock_main.o : $(GMOCK_SRCS_)
+	$(CXX) $(CPPFLAGS) -I$(GTEST_DIR) -I$(GMOCK_DIR) $(CXXFLAGS) \
+            -c $(GMOCK_DIR)/src/gmock_main.cc -o tests/gmock_main.o
+
+tests/gmock.a : tests/gmock-all.o tests/gtest-all.o
+	$(AR) $(ARFLAGS) $@ $^
+
+tests/gmock_main.a : tests/gmock-all.o tests/gtest-all.o tests/gmock_main.o
 	$(AR) $(ARFLAGS) $@ $^
