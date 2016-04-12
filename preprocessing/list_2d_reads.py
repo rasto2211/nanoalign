d8646806bfce9fff8f0cf153f88c576b8b1bb049
Rastislav Rabatin <rastislav.rabatin@gmail.com>
2016-04-12 16:12:03 +0200

    Script for listing all 2D reads.

---
+++ preprocessing/list_2d_reads.py
@@ -0,0 +1,18 @@
+import h5py
+import sys
+import os
+
+folder = sys.argv[1]
+
+if not folder.endswith('/'):
+    folder += '/'
+
+basecall_2d = "Analyses/Basecall_2D_000/BaseCalled_2D/Fastq"
+for filename in os.listdir(folder):
+    if filename.endswith(".fast5"):
+        try:
+            fast5_file = h5py.File(folder + filename, 'r')
+            if basecall_2d in fast5_file:
+                print(filename)
+        except Exception:
+            pass
