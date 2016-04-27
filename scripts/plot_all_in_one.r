eec933c9d66b7a1dd85bc19a586584d928710896
Rastislav Rabatin <rastislav.rabatin@gmail.com>
2016-04-27 20:24:27 +0200

    Update plots after changes in kmers_intersection_seqs_main and kmers_intersection_samples_main

--- scripts/plot_all_in_one.r
+++ scripts/plot_all_in_one.r
@@ -8,40 +8,51 @@ if (length(args)!=3) {
   stop("Args: [intersection.csv] [baseline.csv] [read_name]", call.=FALSE)
 } 
 
-data_samples <- read.csv(args[1], header=F)
-colnames(data_samples) <- c("k","samples","inter")
-data_baselines <- read.csv(args[2], header=F)
-colnames(data_baselines) <- c("k", "inter") 
+data_samples <- read.csv(args[1], header=T)
+data_baselines <- read.csv(args[2], header=T)
 
 n <- 22
 qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
 col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, 
 			    rownames(qual_col_pals)))
 
+# Kmer sizes 9...30 => 22 data points for every sequence.
 viterbi <- data_baselines[1:22,]
 metrichor <- data_baselines[23:44,]
 step_size <- 15
 
 read_name <- args[3]
 
-gg <- ggplot(data_samples, aes(x=samples,y=inter*100, group=factor(k))) + 
+identities <-(data_samples$true_positive/
+	      (data_samples$true_positive+data_samples$false_negative))*100
+x_breaks <- seq(min(data_samples$num_samples), 
+		max(data_samples$num_samples),step_size)
+y_breaks <- seq(round(min(identities)), round(max(identities)),5)
+
+gg <- ggplot(data_samples, 
+	     aes(x=samples,
+		 y=(true_positive/(true_positive+false_negative))*100, 
+		 group=factor(k))) + 
   geom_point(aes(col=factor(k))) + 
   geom_line(aes(group=factor(k),col=factor(k)), show.legend=TRUE) +
-  scale_x_continuous(breaks=seq(min(data_samples$samples),max(data_samples$samples),step_size)) +
-  scale_y_continuous(breaks=seq(round(min(data_samples$inter*100)),
-		      round(max(data_samples$inter*100)),5)) +
-  geom_hline(data=viterbi, aes(yintercept=inter*100, fill="Viterbi", group=k), 
-	      show.legend=TRUE, linetype="dashed", col="red") +
-  geom_hline(data=metrichor,aes(yintercept=inter*100, fill="Metrichor"),
+  scale_x_continuous(breaks=x_breaks) +
+  scale_y_continuous(breaks=y_breaks) +
+  geom_hline(data=viterbi, 
+	     aes(yintercept=(true_positive/(true_positive+false_negative))*100, 
+		 fill="Viterbi", group=k), 
+	     show.legend=TRUE, linetype="dashed", col="red") +
+  geom_hline(data=metrichor,
+	     aes(yintercept=(true_positive/(true_positive+false_negative))*100, 
+		 fill="Metrichor"),
 	      show.legend=TRUE, linetype="dashed", col="black") +
   xlab("Number of samples") +
   ylab("Intersection of kmers with reference (%)") +
   ggtitle(basename(read_name)) +
   scale_colour_manual(values = col_vector) +
   guides(colour = guide_legend(title="Length of kmer"), 
-	  fill = guide_legend(title="Baselines", 
-				override.aes = list(colour=c("black", "red")), 
-				labels=c("Metrichor", "Viterbi"), order=2)) +
- theme(plot.title = element_text(size=12))
+	 fill = guide_legend(title="Baselines", 
+			     override.aes = list(colour=c("black", "red")), 
+			     labels=c("Metrichor", "Viterbi"), order=2)) +
+  theme(plot.title = element_text(size=12))
 
 ggsave(paste(read_name,"_all_in_one.pdf", sep=""), gg)
--- scripts/plot_separate.r
+++ scripts/plot_separate.r
@@ -1,6 +1,5 @@
 library(ggplot2)
 library(grid)
-# library(xtable)
 
 args = commandArgs(trailingOnly=TRUE)
 
@@ -9,10 +8,9 @@ if (length(args)!=3) {
 } 
 
 data_samples <- read.csv(args[1], header=T)
-colnames(data_samples) <- c("k","samples","inter")
-data_baselines <- read.csv(args[2], header=F)
-colnames(data_baselines) <- c("k", "inter") 
+data_baselines <- read.csv(args[2], header=T)
 
+# Kmer sizes 9...30 => 22 data points for every sequence.
 viterbi <- data_baselines[1:22,]
 metrichor <- data_baselines[23:44,]
 step_size <- 15
@@ -20,36 +18,38 @@ step_size <- 15
 read_name <- args[3]
 
 pdf(paste(read_name,"_separate.pdf", sep=""))
+
+# Kmer range 9...30.
 for (i in 9:30) {
+  # Take data for the given kmers of size i.
+  # Take only number of 
   data_k <- data_samples[data_samples$k==i,]
   viterbi_k <- viterbi[viterbi$k==i,]
   metrichor_k <- metrichor[metrichor$k==i,]
 
-  breaks_x <- seq(min(data_k$samples),max(data_k$samples),step_size)
+  breaks_x <- seq(min(data_k$num_samples),max(data_k$num_samples),step_size)
 
-  # Set ticks
-  # min_y <- min(data_k$inter, viterbi_k$inter, metrichor_k$inter)*100
-  # max_y <- max(data_k$inter, viterbi_k$inter, metrichor_k$inter)*100
-  # lim_y_low <- round(min_y)
-  # lim_y_upper <- round(max_y)
-  # y_step <- (lim_y_upper - lim_y_low) / 22
-  # breaks_y <- seq(max(lim_y_low-y_step,0), min(lim_y_upper+y_step,100), y_step)
-
-  gg <- ggplot(data_k, aes(x=samples,y=inter*100)) + 
+  gg <- ggplot(data_k,
+	       aes(x=num_samples, 
+		   y=(true_positive/(true_positive+false_negative))*100)) + 
     geom_point(aes(col="samples"), col="blue") + 
     geom_line(aes(col="samples"), show.legend=TRUE, col="blue") +
     scale_x_continuous(breaks=breaks_x) +
     # scale_y_continuous(breaks=breaks_y) +
-    geom_hline(data=viterbi_k, aes(yintercept=inter*100, fill="viterbi_k", group=k), 
-		show.legend=TRUE, linetype="dashed", col="red") +
-    geom_hline(data=metrichor_k,aes(yintercept=inter*100, fill="metrichor_k"),
-		show.legend=TRUE, linetype="dashed", col="black") +
+    geom_hline(data=viterbi_k, 
+	       aes(yintercept=(true_positive/(true_positive+false_negative))*100, 
+		   fill="viterbi_k", group=k), 
+	       show.legend=TRUE, linetype="dashed", col="red") +
+    geom_hline(data=metrichor_k,
+	       aes(yintercept=(true_positive/(true_positive+false_negative))*100,
+		   fill="metrichor_k"),
+	       show.legend=TRUE, linetype="dashed", col="black") +
     xlab("Number of samples") +
     ylab("Intersection of kmers with reference (%)") +
     guides(colour = guide_legend(title="Length of kmer"), 
-	    fill = guide_legend(title="Baselines", 
-				  override.aes = list(colour=c("black", "red")), 
-				  labels=c("metrichor_k", "viterbi_k"), order=2)) +
+	   fill = guide_legend(title="Baselines", 
+			       override.aes = list(colour=c("black", "red")), 
+			       labels=c("metrichor_k", "viterbi_k"), order=2)) +
     ggtitle(paste(basename(read_name),"\n K=",i,sep=""))
 
   print(gg)
